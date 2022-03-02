5df6add38fd707db2333230935ec4fdad3ed5a71
Kay <62443519+thecrisperson1@users.noreply.github.com>
2022-03-02 21:45:27 +0000

    Deobfuscated the comments lol

--- Main.cpp
+++ Main.cpp
@@ -1,33 +1,32 @@
 #include <iostream>
 #include <SDL.h>
-
+//Variables
 char quit;
 
-//Includes Main 
+
 int main(int argc, char** args) {
 	SDL_Init(SDL_INIT_EVERYTHING);
 
 
-	//Window Moment
+	//Window
 	SDL_Window* window = SDL_CreateWindow("Solaroid", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
 
-	//Make Sure it like did it lol
-	//Using std::cout so i dont have to use namespace std
+	//Window Error
 	if (!window) { std::cout << "Window Did Not Window :(   ERROR: " << SDL_GetError() << std::endl; }
-	//Hopefully /\ works bc idk how to test it
+	
 
 
-	//Get Window surface, like a JFrame canvas idk why im writing this you dont know what that is lol
+	//Window Surface
 	SDL_Surface* windowSurface = SDL_GetWindowSurface(window);
 	
 	
-	//Makes A coloured rectangle which fills the entire surface
+	//Pink Rectangle
 	SDL_FillRect(windowSurface, NULL, SDL_MapRGB(windowSurface->format, 255, 100, 200));
 
 	SDL_UpdateWindowSurface(window);
 	
 	
-	//my primitive quit system bc if you just close the window it steals your RAM so sneaky
+	//Quit
 	std::cout << "Input X into the console to quit ";
 	std::cin >> quit;
 	if (std::tolower(quit) == 'x') {
