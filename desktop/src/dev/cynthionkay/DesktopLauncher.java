dbdd7abc0cf94d81f225a13610a001afa96780e6
ice100k <ice100k@outlook.com>
2022-03-13 12:54:14 +0000

    random messages

--- desktop/src/dev/cynthionkay/DesktopLauncher.java
+++ desktop/src/dev/cynthionkay/DesktopLauncher.java
@@ -4,11 +4,28 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import dev.cynthionkay.Solaroid;
 
+import java.util.Random;
+
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+
+
+
+		String[] messages = {"Kinda Cringe Ngl", "A Sad Game", "For People Without A Life", "Just No", "Imagine having the imagination to write these.."};
+
+		Random r = new Random();
+		int low = 0;
+		int high = messages.length;
+
+		config.setTitle("Solaroid: " + messages[r.nextInt(high-low) + low]);
+
+		config.useVsync(true);
 		config.setForegroundFPS(60);
+
 		new Lwjgl3Application(new Solaroid(), config);
+		
 	}
 }
