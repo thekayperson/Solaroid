8c69ab17f7908784f7b3dbbed49333d10c1d6f20
kay <fuckyou@gmail.com>
2022-03-20 19:52:39 +0000

    refactoring

--- core/src/dev/cynthionkay/Solaroid.java
+++ core/src/dev/cynthionkay/Solaroid.java
@@ -8,18 +8,28 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class Solaroid extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	int x = 0;
+	int y = 0;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("/World/Terrain/green.png");
+		img = new Texture("green.png");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(1, 1, 1, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+
+		for(int i=0; i<1000; i++) {
+			for(int o=0; o<20; o++) {
+				batch.draw(img, x, y);
+				y += 32;
+			}
+			x += 32;
+		}
+		x=0;
+		y=0;
 		batch.end();
 	}
 	
