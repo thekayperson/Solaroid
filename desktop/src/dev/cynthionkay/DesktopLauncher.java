349429c92d3aa11ae3d349c11e5676b7e1196fe8
ice100k <ice100k@outlook.com>
2022-03-20 09:26:21 +0000

    os checking

--- desktop/src/dev/cynthionkay/DesktopLauncher.java
+++ desktop/src/dev/cynthionkay/DesktopLauncher.java
@@ -1,9 +1,13 @@
 package dev.cynthionkay;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import dev.cynthionkay.Solaroid;
 
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.util.List;
 import java.util.Random;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
@@ -11,8 +15,6 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-
-
 
 		String[] messages = {"Kinda Cringe Ngl", "A Sad Game", "For People Without A Life", "Just No", "Imagine having the imagination to write these.."};
 
@@ -24,6 +26,50 @@ public class DesktopLauncher {
 
 		config.useVsync(true);
 		config.setForegroundFPS(60);
+
+		List<String> args = ManagementFactory.getRuntimeMXBean().getInputArguments();
+		System.out.println("Runtime Arguments: " + args);
+
+		String Op = System.getProperty("os.name");
+		System.out.println("Application Started Using: " + Op);
+
+		if (Op.equals("Mac OS X")){
+
+			//Mac stuff here
+			System.out.println("Macs are kinda cringe ngl");
+
+			if (!args.contains("-XstartOnFirstThread")) {
+
+				System.out.println("look now i need to kill myself to care for your special needs :(");
+
+				Runtime runtime = Runtime.getRuntime();
+
+				try {
+					Process proc = runtime.exec("java -jar Solaroid.jar");
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+				System.exit(0);
+
+			}
+
+
+		}
+
+		if (Op.equals("Windows 10")){
+
+			//Windows stuff here
+			System.out.println("Imagine Michaelsoft Binbows 10");
+
+		}
+
+		if (Op.equals("Linux")){
+
+			//Linux stuff here
+			System.out.println("Application Accepted!");
+
+		}
 
 		new Lwjgl3Application(new Solaroid(), config);
 		
