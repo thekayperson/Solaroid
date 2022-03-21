13d0f581218b3bd6a3f185c7cf6e4cf4b16da548
ice100k <ice100k@outlook.com>
2022-03-21 18:50:41 +0000

    bruh

--- desktop/src/dev/cynthionkay/DesktopLauncher.java
+++ desktop/src/dev/cynthionkay/DesktopLauncher.java
@@ -16,7 +16,7 @@ public class DesktopLauncher {
 		config.setWindowIcon("Ui/Icons/logoalt.png");
 
 		//we should make this like a file at some point
-		String[] messages = {"sneaky", "Kinda Cringe Ngl", "A Sad Game", "For People Without A Life", "Just No", "Imagine having the imagination to write these..", "You did this.", "Why are we here?", "Skill issue.", "You really think you can do this?", "'>,< Doubles", "^.^ Floats", "For elite gamers", ">.<", "Powered by java", "photocynthesis", "o-kay'", "Imagine having the imagination to be able to imagine having the imagination to write these.."};
+		String[] messages = {"sneaky", "Kinda Cringe Ngl", "A Sad Game", "For People Without A Life", "Just No", "Imagine having the imagination to write these..", "You did this.", "Why are we here?", "Skill issue.", "You really think you can do this?", "'>,< Doubles", "^.^ Floats", "For elite gamers", ">.<", "Powered by java", "photocynthesis", "o-kay'", "Imagine having the imagination to be able to imagine having the imagination to write these..", "bruh"};
 
 		Random r = new Random();
 		int low = 0;
