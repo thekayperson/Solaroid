b37f0ec3e8fc1d07c87df9acc060f5c922804fe5
thecrisperson1 <christopherolson06@outlook.com>
2022-04-05 19:51:47 +0100

    got a bunch of stuff done i cannot be asked to write it all so fuck you

--- core/src/dev/cynthionkay/Solaroid.java
+++ core/src/dev/cynthionkay/Solaroid.java
@@ -1,36 +1,56 @@
 package dev.cynthionkay;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Cursor;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 
 public class Solaroid extends ApplicationAdapter {
+
+
 	SpriteBatch batch;
 	Texture grass;
 	Texture water;
-	FileRead readFile = new FileRead("file.txt");
+	Texture sand;
+	SaveFileRead readFile = new SaveFileRead("file.txt");
 	@Override
 	public void create() {
+
 		batch = new SpriteBatch();
 		grass = new Texture("grass.png");
 		water = new Texture("water.png");
-		System.out.println(readFile.theDEETS);
+		sand = new Texture("sand.png");
 
 	}
 
 	@Override
 	public void render() {
+
 		int DEETSValue = 0;
 		batch.begin();
-		for(int i = 0; i<readFile.theDEETS.size() / 2; i++){
-			int x = Integer.parseInt(readFile.theDEETS.get(DEETSValue));
+		for(int i = 0; i<readFile.finalDEETS.size() / 3; i++){
+			int x = Integer.parseInt(readFile.finalDEETS.get(DEETSValue));
 			DEETSValue += 1;
-			int y = Integer.parseInt(readFile.theDEETS.get(DEETSValue));
-			batch.draw(grass, x, y);
+			int y = Integer.parseInt(readFile.finalDEETS.get(DEETSValue));
 			DEETSValue += 1;
+			int blockType = Integer.parseInt(readFile.finalDEETS.get(DEETSValue));
+			DEETSValue += 1;
+			if(blockType == 1){batch.draw(grass, x, y);}
+			else if(blockType == 2){batch.draw(water, x, y);}
+			else if(blockType == 3){batch.draw(sand, x, y);}
 		}
+		/*
+		Pixmap cursorMap = new Pixmap(Gdx.files.internal("cursor_1.png"));
+		int xHotSpot = 55;
+		int yHotSpot = 276;
+		Cursor cursor = Gdx.graphics.newCursor(cursorMap, xHotSpot, yHotSpot);
+		Gdx.graphics.setCursor(cursor);
+		cursorMap.dispose();
+		*/
 
 		ScreenUtils.clear(1, 1, 1, 1);
 
@@ -40,8 +60,10 @@ public class Solaroid extends ApplicationAdapter {
 	}
 	@Override
 	public void dispose () {
+
 		batch.dispose();
 		grass.dispose();
 		water.dispose();
 	}
+
 }
---
+++ core/src/dev/cynthionkay/StandardFileRead.java
@@ -0,0 +1,57 @@
+package dev.cynthionkay;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+//Please dont read this it is a travesty of justice
+//At some point i will use this file to make the list of messages into a file but not right now
+public class StandardFileRead {
+
+
+    ArrayList<String> theDEETS = new ArrayList<String>();
+    ArrayList<String> finalDEETS = new ArrayList<>();
+    public int i = 0;
+    public StandardFileRead(String fileName){
+
+        //This is a variable
+
+        try{
+            //Reads all lines of file and inserts into an array
+            int i = 1;
+            File fileToRead = new File(fileName);
+            Scanner fileReader = new Scanner(fileToRead);
+            while(fileReader.hasNextLine()){
+                //this might be a variable?
+                String currentData = fileReader.nextLine();
+                theDEETS.add(currentData);
+
+            }
+            fileReader.close();
+            /* this is literally just the save file reader but with some parts commented out for simple file reading
+
+            //String should be split here
+            for(int j = 0;j<theDEETS.size(); j++) {
+                String currentIterationSplit = theDEETS.get(j);
+                String[] parts = currentIterationSplit.split("/");
+                String part1 = parts[0];
+                String part2 = parts[1];
+                String part3 = parts[2];
+                finalDEETS.add(part1);
+                finalDEETS.add(part2);
+                finalDEETS.add(part3);
+            }
+            System.out.println(finalDEETS);
+            */
+        }
+        catch(FileNotFoundException EveryoneCallsTheirExceptionEButImTooCoolForThat){
+
+            System.out.println("lmao fucking dumbass you cant even read a file.");
+            EveryoneCallsTheirExceptionEButImTooCoolForThat.printStackTrace();
+
+        }
+
+
+    }
+
+
+}
