e00462a07bfddbeb5821f7942f77c7a080d354da
thecrisperson1 <christopherolson06@outlook.com>
2022-04-07 13:29:38 +0100

    i need to do this again

--- core/src/dev/cynthionkay/Solaroid.java
+++ core/src/dev/cynthionkay/Solaroid.java
@@ -6,7 +6,9 @@ import com.badlogic.gdx.graphics.Cursor;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.MapRenderer;
+import com.badlogic.gdx.maps.tiled.*;
+import com.badlogic.gdx.scenes.scene2d.ui.Cell;
 import com.badlogic.gdx.utils.ScreenUtils;
 import dev.cynthionkay.utils.SaveFileRead;
 import com.badlogic.gdx.ApplicationAdapter;
@@ -17,29 +19,19 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
+import java.awt.image.BufferedImage;
 
 
-public class Solaroid extends ApplicationAdapter implements InputProcessor {
-	Texture img;
-	TiledMap tiledMap;
-	OrthographicCamera camera;
-	TiledMapRenderer tiledMapRenderer;
+public class Solaroid extends ApplicationAdapter {
+
+
+
 
 	@Override
 	public void create () {
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
 
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false,w,h);
-		camera.update();
-		tiledMap = new TmxMapLoader().load("tilemap.tmx");
-		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
-		Gdx.input.setInputProcessor(this);
 	}
 
 	@Override
@@ -47,66 +39,7 @@ public class Solaroid extends ApplicationAdapter implements InputProcessor {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		camera.update();
-		tiledMapRenderer.setView(camera);
-		tiledMapRenderer.render();
-	}
 
-	@Override
-	public boolean keyDown(int keycode) {
-		if(keycode == Input.Keys.LEFT)
-			camera.translate(-16,0);
-		if(keycode == Input.Keys.RIGHT)
-			camera.translate(16,0);
-		if(keycode == Input.Keys.UP)
-			camera.translate(0,16);
-		if(keycode == Input.Keys.DOWN)
-			camera.translate(0,-16);
-
-
-
-		return false;
-	}
-
-	@Override
-	public boolean keyUp(int keycode) {
-
-		if(keycode == Input.Keys.NUM_1)
-			tiledMap.getLayers().get(0).setVisible(!tiledMap.getLayers().get(0).isVisible());
-		if(keycode == Input.Keys.NUM_2)
-			tiledMap.getLayers().get(1).setVisible(!tiledMap.getLayers().get(1).isVisible());
-		return false;
-	}
-
-	@Override
-	public boolean keyTyped(char character) {
-
-		return false;
-	}
-
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		return false;
-	}
-
-	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		return false;
-	}
-
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		return false;
-	}
-
-	@Override
-	public boolean mouseMoved(int screenX, int screenY) {
-		return false;
-	}
-
-	@Override
-	public boolean scrolled(float amountX, float amountY) {
-		return false;
 	}
 
 
