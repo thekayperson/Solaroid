fc50694c67f676ec442beb3e11ca2d51314dbed6
kay <fuckyou@gmail.com>
2022-04-17 12:38:04 +0100

    haxe

--- README.md
+++
@@ -1,3 +0,0 @@
-# Solaroid
-This isnt a game yet but it will be at some point <br />
-Everything has to start somewhere right? 
--- core/src/dev/cynthionkay/save/Save.java
+++
@@ -1,9 +0,0 @@
-package dev.cynthionkay.save;
-
-import dev.cynthionkay.generation.PerlinNoise;
-
-//This class is used for generating save files
-public class Save {
-
-
-}
