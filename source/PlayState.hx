0df8a127ed0040f34e1ba06cd49a9ee14046b735
kay <fuckyou@gmail.com>
2022-04-17 12:38:17 +0100

    haxe

---
+++ source/PlayState.hx
@@ -0,0 +1,19 @@
+package;
+
+import flixel.FlxState;
+import flixel.text.FlxText;
+
+class PlayState extends FlxState
+{
+	override public function create()
+	{
+		var text = new FlxText(10, 10, 100, "bruh");
+		add(text);
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
